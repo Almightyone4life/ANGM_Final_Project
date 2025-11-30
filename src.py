9442b1a7a42c7cb1b7135c62bfec8db75d24864a
Brandon Hutson <nyancatdoge@gmail.com>
2025-11-30 14:35:18 -0600

    added player and boss

--- src.py
+++ src.py
@@ -3,15 +3,16 @@ import math
 import pygame
 
 class Boss:
-    def __init__(self, name, health):
+    def __init__(self, name, health, weapon):
         self.name = name
         self.health = health
+        self.weapon = weapon
 
     def take_damage(self, amount):
         self.health -= amount
 
-    def deal_damage(self, player):
-        pass
+    def deal_damage(self, player, amount):
+        player.take_damage(amount)
 
     def heal_damage(self, amount):
         self.health += amount
@@ -38,8 +39,17 @@ class Player:
         print(f"Current health: {self.health}")
         return True if self.health > 0 else False
     
+player = Player("Hero", 200, "Sword")
+boss = Boss("Dragon", 300, "Claws")
+
+player_damage = 20
+boss_damage = 25
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    #player attack input
+    player.deal_damage(boss, player_damage + (random.randint(-2, 5)))
