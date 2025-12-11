d5fd1c88e443a24d7a0062da6d82ff7cc5180e4f
Brandon Hutson <nyancatdoge@gmail.com>
2025-12-10 20:16:09 -0600

    added more boss moves

--- src.py
+++ src.py
@@ -15,7 +15,7 @@ class Boss:
     def deal_damage(self, amount: int):
         return amount + (random.randint(-5, 3))
 
-    def heal_damage(self, amount):
+    def heal_damage(self, amount: int):
         self.health += amount
     
     def is_alive(self):
@@ -44,7 +44,8 @@ player = Player("Hero", 200, "Sword")
 boss = Boss("Dragon", 300, "Claws")
 
 player_damage = 20
-boss_damage = 20
+player_special = 40
+boss_damage = 30
 
 
 
@@ -89,7 +90,7 @@ def play_game():
         return 0
     
 def play_round():
-    player_pick = input("Player's turn: A = Attack, H = heal, P = Surrender\n")
+    player_pick = input("Player's turn: A = Attack, S = Special, H = heal, P = Surrender\n")
     if player_pick.lower() == 'a':
         player.deal_damage(boss, player_damage + (random.randint(-2, 5)))
         #boss.take_damage(damage)
@@ -97,8 +98,22 @@ def play_round():
         for char in Att_msg:
             print(char, end='', flush=True)
             time.sleep(0.03)
+    elif player_pick.lower() == 's':
+        Pchance = random.randint (1, 5)
+        if Pchance > 2:
+            player.deal_damage(boss, player_special)
+            Spc_msg = (f"{player.name} uses a special attack on the {boss.name}\n")
+            for char in Spc_msg:
+                print(char, end='', flush=True)
+                time.sleep(0.03)
+        else:
+            Smiss_msg = (f"{player.name}'s special attack missed.\n")
+            for char in Smiss_msg:
+                print(char, end='', flush=True)
+                time.sleep(0.03)
+        
     elif player_pick.lower() == 'h':
-        player.heal_damage(20 + (random.randint(1, 5)))
+        player.heal_damage(25 + (random.randint(0, 15)))
     elif player_pick.lower() == 'p':
         player.take_damage(player.health)
         Surr_msg = (f"{player.name} has surrendered!\n")
@@ -111,12 +126,35 @@ def play_round():
     for char in Bturn_msg:
         print(char, end='', flush=True)
         time.sleep(0.03)
-    Bdamage = boss.deal_damage(boss_damage)
-    Batt_msg =(f"{boss.name} attacks {player.name}\n")
-    for char in Batt_msg:
-        print(char, end='', flush=True)
-        time.sleep(0.03)
-    player.take_damage(Bdamage)
+    chance = random.randint (1, 3)
+    if boss.health > 50:
+        if chance != 1:
+            Bdamage = boss.deal_damage(boss_damage)
+            Batt_msg =(f"{boss.name} attacks {player.name}\n")
+            for char in Batt_msg:
+                print(char, end='', flush=True)
+                time.sleep(0.03)
+            player.take_damage(Bdamage)
+        else:
+            Bmiss_msg = (f"{boss.name} missed their attack.\n")
+            for char in Bmiss_msg:
+                print(char, end='', flush=True)
+                time.sleep(0.03)
+    else:
+        des_chance = random.randint (1, 4)
+        if des_chance > 1:
+            Bdamage = boss.deal_damage(boss_damage)
+            Batt_msg =(f"{boss.name} attacks {player.name}\n")
+            for char in Batt_msg:
+                print(char, end='', flush=True)
+                time.sleep(0.03)
+            player.take_damage(Bdamage)
+        else:
+            boss.heal_damage(25 + (random.randint(0, 25)))
+            Bheal_msg = (f"{boss.name} desperately heals some of its wounds.\n")
+            for char in Bheal_msg:
+                print(char, end='', flush=True)
+                time.sleep(0.03)
     
 
 def round_results():
@@ -128,6 +166,7 @@ def round_results():
     for char in Bhealth_msg:
         print(char, end='', flush=True)
         time.sleep(0.03)
+    print("")
 
 if __name__ == "__main__":
     main()
